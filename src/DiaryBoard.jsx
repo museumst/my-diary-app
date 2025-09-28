fafa28a726933c95dafe7c34806d7cee6cf39e59
museumst <museumst@gmail.com>
2025-09-29 08:13:15 +0900

    Update DiaryBoard.jsx

--- src/DiaryBoard.jsx
+++ src/DiaryBoard.jsx
@@ -622,7 +622,7 @@ const DiaryBoard = () => {
                   }}
                   className="text-sm text-blue-500 hover:text-blue-600"
                 >
-                  {isSignupMode ? '이미 계정이 있으신가요? 로그인' : '계정이 없으신가요? 회원가입'}
+                  {isSignupMode ? '이미 계정이 있으신가요? 로그인' : ''}
                 </button>
               </div>
             </div>
